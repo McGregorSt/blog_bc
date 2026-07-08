f26defcd0923ce71dc9578aa814ce456823e4fea
mcgregorst <stasiak.gregor@gmail.com>
2026-07-08 08:05:01 +0200

    db conn

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('blog_app', 'postgres', 'root', {
+const sequelize = new Sequelize('blog_bc', 'blog_user', 'Blazini8311', {
   dialect: 'postgres',
   host: 'localhost',
   logging: false
